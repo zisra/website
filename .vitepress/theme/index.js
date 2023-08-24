de7b0839fb789302de1a4e4bf7cf66d736d3d128
Isra <byzkk@protonmail.com>
2023-08-24 09:57:02 -0500

    Revert "Revert"
    This reverts commit 4bf10573296f9df1e3840cecee0f8479b2f3f5a9.

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,7 +1,6 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import NotFound from './NotFound.vue'
-import LandingPage from './LandingPage.vue'
 import './custom.css'
 
 export default {
@@ -9,7 +8,6 @@ export default {
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'not-found': () => h(NotFound),
-      'landing-page': () => h(LandingPage),
     })
   },
 }
